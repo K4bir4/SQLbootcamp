5bc52a3f7f13b80da5d944a3ae97863f48921801
Kabir Pakhale <115333903+K4bir4@users.noreply.github.com>
2025-02-12 21:36:42 +0530

    Add files via upload

---
+++ folder/SQLQuery2.sql
@@ -0,0 +1,16 @@
+
+select * from Employees
+
+select firstname from Employees
+
+
+select firstname,Salary from Employees
+
+select FirstName,LastName from Employees
+
+select EmployeeID,CONCAT (firstname,' ',LastName) from Employees
+
+select EmployeeID,CONCAT (firstname,' ',LastName) [Fullname] from Employees
+
+select EmployeeID,CONCAT (firstname,' ',LastName) as [Fullname] from Employees
+
