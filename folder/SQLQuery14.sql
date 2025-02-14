2ca2a3dbdaf7ee4ec81ddd009063ff29e6deaa23
Kabir Pakhale <115333903+K4bir4@users.noreply.github.com>
2025-02-14 23:24:23 +0530

    Create SQLQuery14.sql

---
+++ folder/SQLQuery14.sql
@@ -0,0 +1,22 @@
+
+
+--writing a single line comment 
+select * from dbo.Employees
+
+/*
+hi we 
+are learing 
+to comment multi line commentn
+*/
+
+select * from dbo.employeerecords
+
+
+select top 2 * from employees
+select top 5 * from employees
+
+select top 2 employeeid, salary from employees
+
+select * from employees
+select top 4 firstname, lastname from employees
+
