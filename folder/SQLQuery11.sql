6e67dfa853aa0869746fa615cc970cc9f870e400
Kabir Pakhale <115333903+K4bir4@users.noreply.github.com>
2025-02-14 23:22:58 +0530

    Create SQLQuery11.sql

---
+++ folder/SQLQuery11.sql
@@ -0,0 +1,16 @@
+
+
+select * from dbo.employees
+
+insert into dbo.employees
+values (10,'Jay','','IT',92000,'2023-01-23')
+
+insert into dbo.employees
+values (11,'Nitin','Sahamai','0',92000,'2023-01-23')
+
+select * from dbo.Employees
+where Department = NULL
+
+select * from dbo.Employees where Department is null
+
+select * from dbo.Employees where Department is not null
