71f11b514c4b23dad18eb416a3b6be9327de15db
Kabir Pakhale <115333903+K4bir4@users.noreply.github.com>
2025-02-16 23:49:21 +0530

    Create SQLQuery35.sql

---
+++ folder/SQLQuery35.sql
@@ -0,0 +1,22 @@
+
+--SQL data typesss
+
+--STRING DATA
+--char, varhcar,nchar,nvarchar
+
+-- either m or f, char (1)
+--varchar (90) student names
+--nchar supports unicodechar
+-- nvarchar(256) supports unicode
+
+--NUMBER TYPE
+--int,big int, tinyint, decimal(p,s),smallint
+
+--age of students, 0-255
+--age of building or trees smallint 
+--decimal (p,s), p=total digits, s= number of digits after decimal point
+--999.99 , decimal (5,2)
+
+--DATE data type , TIME data type , DATETIME data type
+
+
